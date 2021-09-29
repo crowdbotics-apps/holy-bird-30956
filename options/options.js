1b02feb2b4ff6f36551911e4d00f7aea37bb11c0
Crowdbotics <team@crowdbotics.com>
2021-09-29 15:31:59 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "holy_bird_30956",
+  url: "https://holy_bird_30956.botics.co",
+  api: "https://holy_bird_30956.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
